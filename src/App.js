4f49b5c15a4b651802a9203a134e76657228ea6e
Umeh Amaka <amakaumeh01@gmail.com>
2020-06-14 13:08:08 +0100

    Changed font color

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function App() {
         {(typeof weather.main != "undefined") ? (
           <div>
             <div className="location-box">
-  <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="location">{weather.name}, {weather.sys.country}</div>
           <div className="date">{dateBuilder(new Date())}</div>
         </div>
         <div className="weather-box">
